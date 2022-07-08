483373278f064a692b04d9e98d59a03c814dad7b
hermioneee2 <hermioneee2@gmail.com>
2022-07-08 19:32:47 +0900

    login step1 basic UI (name/email) + fonts

---
+++ app/src/main/java/com/example/cs496week2/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cs496week2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+
+}
