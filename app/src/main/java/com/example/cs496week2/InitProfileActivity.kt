7238928a3b283a5eab7f7804778747b653982861
hermioneee2 <hermioneee2@gmail.com>
2022-07-08 23:49:55 +0900

    login step1 show profile picture

--- app/src/main/java/com/example/cs496week2/InitProfileActivity.kt
+++ app/src/main/java/com/example/cs496week2/InitProfileActivity.kt
@@ -2,17 +2,11 @@ package com.example.cs496week2
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
 import com.example.cs496week2.databinding.ActivityInitProfileBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class InitProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityInitProfileBinding
@@ -32,16 +26,24 @@ class InitProfileActivity : AppCompatActivity() {
         //set text using info from kakao account
         if (intent.hasExtra("nameKey")) {
             binding.etName.setText(intent.getStringExtra("nameKey"))
-            Log.d("name", intent.getStringExtra("nameKey").toString())
+//            Log.d("name", intent.getStringExtra("nameKey").toString())
         } else {
             Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
         }
 
         if (intent.hasExtra("emailKey")) {
             binding.etEmail.setText(intent.getStringExtra("emailKey"))
-            Log.d("email", intent.getStringExtra("emailKey").toString())
+//            Log.d("email", intent.getStringExtra("emailKey").toString())
         } else {
-            Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "전달된 이메일이 없습니다", Toast.LENGTH_SHORT).show()
+        }
+
+        if (intent.hasExtra("photoSrcKey")) {
+//            Log.d("email", intent.getStringExtra("photoSrcKey").toString())
+            val imageStr = intent.getStringExtra("photoSrcKey").toString()
+            Glide.with(this).load(imageStr).into(binding.ivProfilePic)
+        } else {
+            Toast.makeText(this, "전달된 포토소스가 없습니다", Toast.LENGTH_SHORT).show()
         }
 
         // set on-click listener
@@ -51,11 +53,10 @@ class InitProfileActivity : AppCompatActivity() {
             val email = binding.etEmail.text;
             Toast.makeText(this@InitProfileActivity, name, Toast.LENGTH_LONG).show()
 
-            // your code to validate the user_name and password combination
-            // and verify the same
+            // send data
 
         }
-
-
     }
+
+
 }
