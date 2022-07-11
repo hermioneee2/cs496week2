a33bcfb57b741b271019432adabeeb750cb2a3cd
hermioneee2 <hermioneee2@gmail.com>
2022-07-11 19:18:19 +0900

    search result shown only when something is typed

--- app/src/main/java/com/example/cs496week2/LoginActivity.kt
+++ app/src/main/java/com/example/cs496week2/LoginActivity.kt
@@ -32,32 +32,50 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-//        // If already logged in once then clicking is skipped
-//        // comment out for coding purpose,
+        // If already logged in once then clicking is skipped
+        // comment out for coding purpose,
 
-//        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
-//            if (error != null) {
-//                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
-//            }
-//            else if (tokenInfo != null) {
-//                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
-//                Log.d("token", "FIRST")
-//                UserApiClient.instance.me { user, error ->
-//                    if (error != null) {
-//                        Log.e("callUserInfo2", "사용자 정보 요청 실패2", error)
-//                    }
-//                    else if (user != null) {
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
+            }
+            else if (tokenInfo != null) {
+                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+                Log.d("token", "FIRST")
+                UserApiClient.instance.me { user, error ->
+                    if (error != null) {
+                        Log.e("callUserInfo2", "사용자 정보 요청 실패2", error)
+                    }
+                    else if (user != null) {
+                        userID = user.id.toString();
+                        name = user.kakaoAccount?.profile?.nickname.toString();
+                        email = user.kakaoAccount?.email.toString();
+                        photoSrc = user.kakaoAccount?.profile?.thumbnailImageUrl.toString();
+                        Log.i("callUserInfo", "사용자 정보 요청 성공" +
+                                "\n회원번호: $userID" +
+                                "\n닉네임: $name" +
+                                "\n이메일: $email" +
+                                "\n프로필사진: $photoSrc")
+                        val intent = Intent(this, InitProfileActivity::class.java)
+                        intent.putExtra("userIDKey", userID)
+                        intent.putExtra("nameKey", name)
+                        intent.putExtra("emailKey", email)
+                        intent.putExtra("photoSrcKey", photoSrc)
+                        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                        finish()
+//
 //                        Log.i("callUserInfo2", "사용자 정보 요청 성공2" +
 //                                "\n회원번호: ${user.id}" +
 //                                "\n이메일: ${user.kakaoAccount?.email}" +
 //                                "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-//                                "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")                    }
-//                }
-//                val intent = Intent(this, InitializingProfileActivity::class.java)
+//                                "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                    }
+                }
+//                val intent = Intent(this, InitProfileActivity::class.java)
 //                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
 //                finish()
-//            }
-//        }
+            }
+        }
 
         //Callback for error cases
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
--- app/src/main/java/com/example/cs496week2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cs496week2/ui/home/HomeFragment.kt
@@ -2,13 +2,11 @@ package com.example.cs496week2.ui.home
 
 import android.content.ClipData
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.widget.Filter
-import android.widget.Filterable
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -26,11 +24,11 @@ class HomeFragment : Fragment(), ItemAdapter.ClickedItem {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    //한다리+한다리건너 지인 정보 모두 리턴
     var itemListModal = arrayOf(
         ItemModal("https://k.kakaocdn.net/dn/bER0sf/btry33hyOgb/Y2R8LMaVEhbgcvq5KCK110/img_110x110.jpg", "Apple", "apple desc"),
         ItemModal("https://k.kakaocdn.net/dn/bER0sf/btry33hyOgb/Y2R8LMaVEhbgcvq5KCK110/img_110x110.jpg", "Banana", "Banana desc"),
         ItemModal("https://k.kakaocdn.net/dn/bER0sf/btry33hyOgb/Y2R8LMaVEhbgcvq5KCK110/img_110x110.jpg", "Orange", "Orange desc")
-
 
     )
 
@@ -60,8 +58,12 @@ class HomeFragment : Fragment(), ItemAdapter.ClickedItem {
         binding.recyclerView.layoutManager = LinearLayoutManager(context);
         binding.recyclerView.setHasFixedSize(true)
 
+//        Log.d("itemModalList.size", itemModalList.size.toString())
+
         itemAdapter = ItemAdapter(this);
         itemAdapter!!.setData(itemModalList)
+
+//        Log.d("itemModalList.size", itemModalList.size.toString())
 
         binding.recyclerView.adapter = itemAdapter
 
@@ -104,7 +106,16 @@ class HomeFragment : Fragment(), ItemAdapter.ClickedItem {
 
             override fun onQueryTextChange(p0: String?): Boolean {
 //                Log.e("TAG", "=> $p0")
-                itemAdapter!!.filter.filter(p0);
+                if (p0 == null || p0.isEmpty()){
+//                    _binding!!.tvHome.text = "empty"
+                    _binding!!.recyclerView.visibility = View.GONE
+
+                } else {
+//                    _binding!!.tvHome.text = "something"
+                    _binding!!.recyclerView.visibility = View.VISIBLE
+                    itemAdapter!!.filter.filter(p0);
+                }
+
                 return true
             }
 
