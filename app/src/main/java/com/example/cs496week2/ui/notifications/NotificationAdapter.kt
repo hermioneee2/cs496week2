8611ae94820547c0aa7317c36449671a85ef57ff
2018008777 <2018008777@hanyang.ac.kr>
2022-07-12 17:44:42 +0900

    Now can remove temp links.

--- app/src/main/java/com/example/cs496week2/ui/notifications/NotificationAdapter.kt
+++ app/src/main/java/com/example/cs496week2/ui/notifications/NotificationAdapter.kt
@@ -10,11 +10,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.cs496week2.R
+import com.example.cs496week2.interfaces.GetUserAPI
+import com.example.cs496week2.objects.MyProfile
+import com.example.cs496week2.objects.RetrofitHelper
 import com.example.cs496week2.ui.home.ItemAdapter
 import com.example.cs496week2.ui.home.ItemModal
 import com.example.cs496week2.ui.home.ProfileCircleAdapter
 import com.example.cs496week2.ui.home.TagAdapter
 import kotlinx.android.synthetic.main.row_notifications.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class NotificationAdapter(
     var context: Context
@@ -50,11 +57,25 @@ class NotificationAdapter(
         holder.description.text = mydesc
 
         holder.btnConnect.setOnClickListener{
+            val getUserAPI = RetrofitHelper.getInstance().create(GetUserAPI::class.java)
+            GlobalScope.launch {
+                val result = getUserAPI.getUserList(
+                    param1 = threeItemModal[0].userID,
+                    param2 = threeItemModal[1].userID,
+                    body = listOf("delete")
+                )
+                withContext(Dispatchers.Main) {
+                    Log.d("ItemActivity remove temp link: ", result.body().toString())
+                    notifyDataSetChanged()
+                }
+            }
+
             // send SMS directly
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + threeItemModal[0].phone))
-            intent.putExtra("sms_body", "[한다리건너]\n 안녕하세요, ${threeItemModal[0].name}님! 제 지인인 ${threeItemModal[1].name}님이 ${threeItemModal[0].name}님을 알고 싶어해요. 그의 연락처는 ${threeItemModal[1].phone}이에요.")
+            intent.putExtra("sms_body", "[한다리건너]\n 안녕하세요, ${threeItemModal[1].name}님! 제 지인인 ${threeItemModal[0].name}님이 ${threeItemModal[1].name}님을 알고 싶어해요. 그의 연락처는 ${threeItemModal[0].phone}이에요.")
             context.startActivity(intent)
         }
+
     }
 
     override fun getItemCount(): Int {
